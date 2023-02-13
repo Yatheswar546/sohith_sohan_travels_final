0f8e65163b417a93f0be670cb14c91df58f01a82
Yatheswar546 <yatheswar546@gmail.com>
2023-02-14 04:10:25 +0530

    sst15

--- js/script.js
+++ js/script.js
@@ -1,30 +1,34 @@
-$(document).ready(function(){
-    $(document).ready(function(){
-
-      $('.buttons').click(function(){
-  
-          $(this).addClass('active').siblings().removeClass('active');
-  
-          var filter = $(this).attr('data-filter')
-  
-          if(filter == 'all'){
-              $('.image').show(400);
-          }else{
-              $('.image').not('.'+filter).hide(200);
-              $('.image').filter('.'+filter).show(400);
-          }
-  
-      });
-  
-      $('.gallery').magnificPopup({
-  
-          delegate:'a',
-          type:'image',
-          gallery:{
-              enabled:true
-          }
-  
-      });
-  
-    });
-}); 
+$('.images').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: false
+            }
+        }, 
+        {
+            breakpoint: 980,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 700,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
